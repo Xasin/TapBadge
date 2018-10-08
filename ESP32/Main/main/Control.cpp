74625e044cafee6eda3e01c14804d329a7672bc4
David Bailey <davidbailey.2889@gmail.com>
2018-10-08 16:21:04 +0200

    Small timer type change

--- ESP32/Main/main/Control.cpp
+++ ESP32/Main/main/Control.cpp
@@ -11,18 +11,22 @@ namespace Touch {
 
 Control::Control(touch_pad_t padNo) : padNo(padNo) {
 	touch_pad_init();
-	touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
+	//touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
+	touch_pad_set_fsm_mode(TOUCH_FSM_MODE_SW);
 
 	touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
 
 	touch_pad_config(padNo, 0);
 	touch_pad_io_init(padNo);
+
+	touch_pad_set_meas_time(0xffff, 0x07ff);
 }
 
 uint16_t Control::read_raw() {
 	uint16_t output = 0;
 
 	//touch_pad_sw_start();
+	touch_pad_read(padNo, &output);
 	touch_pad_read(padNo, &output);
 
 	return output;
