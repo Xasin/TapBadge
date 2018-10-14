4536ad2eb5ec0b001af4ba4cbbfe3a6549345880
David Bailey <davidbailey.2889@gmail.com>
2018-10-14 20:06:54 +0200

    BLE Service file added

---
+++ ESP32/Main/components/Bluetooth/include/Service.h
@@ -0,0 +1,54 @@
+/*
+ * Service.h
+ *
+ *  Created on: 9 Oct 2018
+ *      Author: xasin
+ */
+
+#ifndef COMPONENTS_BLUETOOTH_SERVICE_H_
+#define COMPONENTS_BLUETOOTH_SERVICE_H_
+
+#include <vector>
+
+#include "assert.h"
+#include "esp_gatts_api.h"
+
+#include "Characteristic.h"
+
+namespace Peripheral {
+class BLE_Handler;
+
+namespace Bluetooth {
+
+
+class Service {
+private:
+	void register_characteristic(Characteristic *newCharacteristic);
+protected:
+	friend BLE_Handler;
+
+	std::vector<Characteristic *> characteristics;
+	BLE_Handler * const handler;
+
+	uint16_t handle;
+
+	void set_handle(uint16_t handle);
+
+public:
+	esp_gatt_srvc_id_t id;
+
+	Service(BLE_Handler *handler);
+
+	void set_primary(bool primary);
+	void set_uuid16(uint16_t uuid);
+	void set_uuid32(uint32_t uuid);
+
+	void add_char(Characteristic * newChar);
+
+	void install();
+};
+
+} /* namespace Bluetooth */
+} /* namespace Peripheral */
+
+#endif /* COMPONENTS_BLUETOOTH_SERVICE_H_ */
