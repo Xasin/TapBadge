30f39d6a27b89f58beb61ba3902b62e325db7a97
Xasin <davidbailey.2889@gmail.com>
2018-10-23 16:45:54 +0200

    Added 'return to base color' function

--- ESP32/Main/components/NotifyHandler/NotifyHandler.cpp
+++ ESP32/Main/components/NotifyHandler/NotifyHandler.cpp
@@ -16,10 +16,15 @@ NotifyHandler::NotifyHandler(NeoController * const rgb) : rgb(rgb) {
 void NotifyHandler::flash(PatternElement * const pattern, uint8_t num) {
 	lock();
 
+	Color oldC = *(rgb->get(0));
+
 	for(uint8_t i=0; i<num; i++) {
 		(*rgb)[0]->set(pattern[i].tColor);
 		rgb->fadeTransition(pattern[i].fadeDuration);
 	}
+
+	(*rgb)[0]->set(oldC);
+	rgb->fadeTransition(30000);
 
 	release();
 }
