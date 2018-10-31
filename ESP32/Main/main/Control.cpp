34704795056667ad421ae81c292e55dd332efb7c
David Bailey <davidbailey.2889@gmail.com>
2018-10-31 21:54:52 +0100

    Fine tuning of values

--- ESP32/Main/main/Control.cpp
+++ ESP32/Main/main/Control.cpp
@@ -11,21 +11,22 @@ namespace Touch {
 
 void Control::char_detect_isr() {
 	auto stats = touch_pad_get_status();
-	touch_pad_clear_status();
 
 	uint64_t ISRDelay = esp_timer_get_time() - lastIntrTime;
 	lastIntrTime = esp_timer_get_time();
 	if(ISRDelay > 10000)
 		debounceCnt = 0;
 
-	if(++debounceCnt >= 2) {
+	if(++debounceCnt >= 4) {
 		debounceCnt = 0;
 
-		bool touched = (stats == padNo+1);
+		bool touched = (stats != 0b1111111111);
 		touch_pad_set_trigger_mode(touched ? TOUCH_TRIGGER_ABOVE : TOUCH_TRIGGER_BELOW);
 		if(charDetectHandle != nullptr)
 			xTaskNotifyFromISR(charDetectHandle, touched, eSetValueWithOverwrite, nullptr);
 	}
+
+	touch_pad_clear_status();
 }
 
 Control::Control(touch_pad_t padNo) : padNo(padNo), charDetectHandle(nullptr) {
@@ -38,7 +39,7 @@ Control::Control(touch_pad_t padNo) : padNo(padNo), charDetectHandle(nullptr) {
 
 	touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
 
-	touch_pad_config(padNo, 12);
+	touch_pad_config(padNo, 13);
 	touch_pad_io_init(padNo);
 
 	touch_pad_isr_register([](void *args) {
