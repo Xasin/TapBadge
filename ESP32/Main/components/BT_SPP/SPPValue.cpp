ae92799531bbb9bbf9cd99e13d08eb017aa4393a
David Bailey <davidbailey.2889@gmail.com>
2018-11-22 17:52:28 +0100

    Rewrote & added Value-related functions

--- ESP32/Main/components/BT_SPP/SPPValue.cpp
+++ ESP32/Main/components/BT_SPP/SPPValue.cpp
@@ -6,26 +6,25 @@
  */
 
 #include "SPPValue.h"
-
 #include "SPPServer.h"
+
+#include <cstring>
 
 namespace Bluetooth {
 
-SPP_Value::SPP_Value(SPP_Server& server, uint16_t id) : server(server), id(id) {
-
-}
-
-bool SPP_Value::write(void *data, size_t length) {
-	if(!server.is_connected())
-		return false;
-
-	server.write_packet(id, data, length);
-
-	return true;
+SPP_Value::SPP_Value(SPP_Server& server, uint16_t id) :
+		server(server),
+		data_changed(false),
+		id(id),
+		data_location(nullptr), data_length(0),
+		write_into_data(false) {
 }
 
 void SPP_Value::on_reconnect() {
-	puts("System reconnected");
+	if(data_changed) {
+		write(data_location, data_length);
+		data_changed = false;
+	}
 }
 void SPP_Value::accept_data(const void *data, size_t length) {
 	std::string data_str(reinterpret_cast<const char *>(data), length);
@@ -33,6 +32,34 @@ void SPP_Value::accept_data(const void *data, size_t length) {
 	data_str += '\0';
 
 	printf("SPP ID: %d Got raw data: %s\n", id, data_str.data());
+
+	if(write_into_data && length <= data_length && data_location != nullptr)
+		memcpy(data_location, data, length);
+}
+
+
+bool SPP_Value::write(void *data, size_t length) {
+	if(!server.is_connected())
+		return false;
+
+	if(data != nullptr && length != 0)
+		server.write_packet(id, data, length);
+	else
+		server.write_packet(id, 0, 0);
+
+	return true;
+}
+void SPP_Value::write_retained(void *data, size_t length) {
+	if(!write(data, length)) {
+		data_location = data;
+		data_length = length;
+		data_changed = true;
+	}
+}
+
+void SPP_Value::update_retained() {
+	if(!write(data_location, data_length))
+		data_changed = true;
 }
 
 } /* namespace Bluetooth */
--- ESP32/Main/components/BT_SPP/include/SPPValue.h
+++ ESP32/Main/components/BT_SPP/include/SPPValue.h
@@ -19,18 +19,28 @@ class SPP_Value {
 private:
 	SPP_Server& server;
 
-	bool write(void *data, size_t length);
-
 protected:
 	friend SPP_Server;
 
 	virtual void accept_data(const void *data, size_t length);
 	virtual void on_reconnect();
 
+	bool	 data_changed;
+
 public:
 	const uint16_t id;
 
+	void 	*data_location;
+	size_t 	 data_length;
+
+	bool write_into_data;
+
 	SPP_Value(SPP_Server& server, uint16_t id);
+
+	bool write(void *data, size_t length);
+	void write_retained(void *data, size_t length);
+
+	void update_retained();
 };
 
 } /* namespace Bluetooth */
